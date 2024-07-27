4b0acb3c2f5ac1f1f59d73fa4679dc9c0f62fd8d
CUMT-lty <m13182371876@163.com>
2024-07-27 19:43:58 +0800

    first commit

---
+++ oh-springboot-test/src/main/java/com/litianyu/ohsourcecode/ohspringboottest/OhSpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.litianyu.ohsourcecode.ohspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhSpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhSpringbootTestApplication.class, args);
+	}
+
+}
---
+++ oh-springboot-test/src/test/java/com/litianyu/ohsourcecode/ohspringboottest/OhSpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litianyu.ohsourcecode.ohspringboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhSpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
